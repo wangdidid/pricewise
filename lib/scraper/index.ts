a03485b70e364349f32f0c55ef0615b910353ba1
wangdidid <cumt_10114605@163.com>
2025-03-20 10:27:14 +0800

    env

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -10,7 +10,7 @@ export async function scrapeAmazonProduct(url: string) {
   // BrightData proxy configuration
   const username = String(process.env.BRIGHT_DATA_USERNAME);
   const password = String(process.env.BRIGHT_DATA_PASSWORD);
-  const port = 22225;
+  const port = 33335;
   const session_id = (1000000 * Math.random()) | 0;
 
   const options = {
